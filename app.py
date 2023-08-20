78c86945cc5f1442d089995f58e41b4c0b28e171
Abel <79184789+AbelRapha@users.noreply.github.com>
2023-08-20 19:46:25 +0000

    First lines code

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
 import streamlit as st
 
-st.write("Hello World")
+st.write("## Conversor de medidas")
+
+medidas_velocidade_atual = st.selectbox("Selecione o tipo de medida de velocidade atual", ["km/h","m/s"])
+
+medidas_velocidade_futura = st.selectbox("Selecione o tipo de medida de velocidade futura",["km/h","m/s"])
+
+input_velocidade = st.number_input("Digite o valor da velocidade")
+
+def converte_velocidade(velocidade_atual):
+    return velocidade_atual *2
+
+st.write(f"{converte_velocidade(input_velocidade)}")
