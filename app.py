2f0513cba7ad0f710eccdf2c6dc1517d426e2af4
Abel <79184789+AbelRapha@users.noreply.github.com>
2023-08-21 00:05:39 +0000

    update para converter qualquer medida

--- app.py
+++ app.py
@@ -2,28 +2,31 @@ import streamlit as st
 
 st.write("## Conversor de medidas")
 
-medidas_velocidade_atual = st.selectbox("Selecione o tipo de medida de velocidade atual", ["km/h","m/s"])
+medidas_atual = st.selectbox("Selecione o tipo de medida de velocidade atual", ["km/h","m/s"])
 
-medidas_velocidade_futura = st.selectbox("Selecione o tipo de medida de velocidade futura",["km/h","m/s"])
+medidas_futura = st.selectbox("Selecione o tipo de medida de velocidade futura",["km/h","m/s"])
 
-input_velocidade = st.number_input("Digite o valor da velocidade")
+input_medida = st.number_input("Digite o valor da velocidade")
 
-def converte_velocidade(velocidade_atual, velocidade_futura, input_velocidade):
-    match velocidade_atual:
+def converte_medida(medida_atual, medida_futura, input_medida):
+    match medida_atual:
         case "km/h":
-            match velocidade_futura:
+            match medida_futura:
                 case "m/s":
-                    return input_velocidade / 3.6
-                case velocidade_atual:
-                    return  input_velocidade
+                    return input_medida / 3.6
+                case medida_atual:
+                    return  input_medida
         case "m/s": 
-            match velocidade_futura:
+            match medida_futura:
                 case "k/h":
-                    return input_velocidade * 3.6
-                case velocidade_atual:
-                    return input_velocidade
+                    return input_medida * 3.6
+                case medida_atual:
+                    return input_medida
         case default:
             return "Selecione o tipo de medida para converter ou a ser convertida"
 
 
-st.write(f"{converte_velocidade(medidas_velocidade_atual,medidas_velocidade_futura,input_velocidade)}")
+st.write(f"{converte_medida(medidas_atual,medidas_futura,input_medida)}")
+
+st.write("## Conversor de sistema de unidades")
+ 
